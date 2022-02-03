4504144a0c76df90c53c16715a27f4e881180886
kate <katesan945@gmail.com>
2022-02-03 19:38:35 +0700

    remove redaxios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import { useState } from "react";
-import redaxios from "redaxios";
 const Home: NextPage = () => {
   return (
     <div>
